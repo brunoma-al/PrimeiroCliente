02bd26f35a9ee47ac2fc645a7fb925426ba5cc23
Bruno Gabriel <brugacag@gmail.com>
2023-12-13 21:47:10 -0300

    Finalizado.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "primeiro.cliente.cursoandroid"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "primeiro.cliente.cursoandroid"
@@ -30,7 +30,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/primeiro/cliente/cursoandroid/TelaSplashActivity.java
+++ app/src/main/java/primeiro/cliente/cursoandroid/TelaSplashActivity.java
@@ -1,4 +1,32 @@
 package primeiro.cliente.cursoandroid;
 
-public class TelaSplashActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TelaSplashActivity extends AppCompatActivity {
+
+    public static final int TIME_OUT_SPLASH = 15000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        cumutarTelaSplash();
+    }
+
+    private void cumutarTelaSplash() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent telaPrincipal = new Intent(TelaSplashActivity.this, MainActivity.class);
+                startActivity(telaPrincipal);
+                finish();
+            }
+        }, TIME_OUT_SPLASH);
+    }
 }
